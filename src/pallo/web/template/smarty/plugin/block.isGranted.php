0684ed79a6937db85a20e2f011f553dd371c04ba
Joris Vandeweerd <joris@statik.be>
2014-01-22 14:14:08 +0100

    added granted block functions

---
+++ src/pallo/web/template/smarty/plugin/block.isGranted.php
@@ -0,0 +1,32 @@
+<?php
+
+function smarty_block_isGranted($params, $content, &$smarty, &$repeat) {
+    if ($repeat) {
+        return;
+    }
+
+    if (!isset($params['route']) && !isset($params['url']) && !isset($params['permission'])) {
+        throw new Exception('No route, URL or permission provided');
+    }
+
+    $app = $smarty->getTemplateVars('app');
+    if (!isset($app['system'])) {
+        trigger_error('System is not set in the app variable.');
+    }
+
+    $securityManager = $app['system']->getDependencyInjector()->get('pallo\\library\\security\\SecurityManager');
+
+    if (isset($params['route'])) {
+        if ($securityManager->isPathAllowed($params['route'])) {
+            return $content;
+        }
+    } elseif (isset($params['url'])) {
+        if ($securityManager->isUrlAllowed($params['url'])) {
+            return $content;
+        }
+    } elseif ($securityManager->isPermissionGranted($params['permission'])) {
+        return $content;
+    }
+
+    return;
+}
---
+++ src/pallo/web/template/smarty/plugin/block.isNotGranted.php
@@ -0,0 +1,32 @@
+<?php
+
+function smarty_block_isNotGranted($params, $content, &$smarty, &$repeat) {
+    if ($repeat) {
+        return;
+    }
+
+    if (!isset($params['route']) && !isset($params['url']) && !isset($params['permission'])) {
+        throw new Exception('No route or permission provided');
+    }
+
+    $app = $smarty->getTemplateVars('app');
+    if (!isset($app['system'])) {
+        trigger_error('System is not set in the app variable.');
+    }
+
+    $securityManager = $app['zibo']->getDependency('zibo\\library\\security\\SecurityManager');
+
+    if (isset($params['route'])) {
+        if (!$securityManager->isPathAllowed($params['route'])) {
+            return $content;
+        }
+    } elseif (isset($params['url'])) {
+        if (!$securityManager->isUrlAllowed($params['url'])) {
+            return $content;
+        }
+    } elseif (!$securityManager->isPermissionGranted($params['permission'])) {
+        return $content;
+    }
+
+    return;
+}
