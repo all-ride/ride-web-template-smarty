66e4e612536e56d198a44c26659f84c62192038e
Joris Vandeweerd <joris@statik.be>
2014-02-12 15:54:52 +0100

    parameter fix modifier

--- src/pallo/web/template/smarty/plugin/modifier.decorate.php
+++ src/pallo/web/template/smarty/plugin/modifier.decorate.php
@@ -1,18 +1,13 @@
 <?php
 
-function smarty_modifier_decorate($value, array $parameters = null) {
+function smarty_modifier_decorate($value, $type = null) {
     static $decorators = array();
 
-    if (!$parameters) {
-        return $value;
-    }
-
-    $type = array_shift($parameters);
     if (!$type || !is_string($type)) {
         return '<span class="error">Could not decorate value: invalid decorator type provided</span>';
     }
 
-    if (isset($decorators[$type])) {
+    if (!isset($decorators[$type])) {
         global $system;
 
         $decorators[$type] = $system->getDependencyInjector()->get('pallo\\library\\decorator\\Decorator', $type);
