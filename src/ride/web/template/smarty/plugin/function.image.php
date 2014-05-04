e0ca2324f69c3a7c137e61c7231d26f177b6e519
Joris Vandeweerd <joris@statik.be>
2014-05-04 11:28:28 +0200

    replaced String with StringHelper

--- src/ride/web/template/smarty/plugin/function.image.php
+++ src/ride/web/template/smarty/plugin/function.image.php
@@ -1,6 +1,6 @@
 <?php
 
-use ride\library\String;
+use ride\library\StringHelper;
 
 function smarty_function_image($params, &$smarty) {
     try {
@@ -44,15 +44,14 @@ function smarty_function_image($params, &$smarty) {
             unset($params['thumbnail']);
         }
 
-        $src = new String($src);
-        if (!$src->startsWith(array('http://', 'https://'))) {
+        if (!StringHelper::startsWith($src, array('http://', 'https://'))) {
             $app = $smarty->getTemplateVars('app');
             if (!isset($app['system'])) {
                 return '<span style="color: red;">Could not load image ' . $src . ': system is not available in the app variable.</span>';
             }
 
             $imageUrlGenerator = $app['system']->getDependencyInjector()->get('ride\\library\\image\\ImageUrlGenerator');
-            $src = $imageUrlGenerator->generateUrl((string) $src, $thumbnailer, $width, $height);
+            $src = $imageUrlGenerator->generateUrl($src, $thumbnailer, $width, $height);
 
             $params['src'] = $src;
         }
