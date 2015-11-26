bc04fb731864a44a629e9577fb5a8963672d9632
Kaya <kayalion@kayalion.net>
2015-11-26 11:43:56 +0100

    implemented query parameters

--- src/ride/web/template/smarty/plugin/function.url.php
+++ src/ride/web/template/smarty/plugin/function.url.php
@@ -14,6 +14,18 @@ function smarty_function_url($params, &$smarty) {
         unset($params['parameters']);
     }
 
+    $queryParameters = null;
+    if (!empty($params['query'])) {
+        $queryParameters = $params['query'];
+        unset($params['query']);
+    }
+
+    $querySeparator = '&';
+    if (!empty($params['separator'])) {
+        $querySeparator = $params['separator'];
+        unset($params['separator']);
+    }
+
     $var = null;
     if (!empty($params['var'])) {
         $var = $params['var'];
@@ -26,7 +38,7 @@ function smarty_function_url($params, &$smarty) {
     }
 
     $router = $app['system']->getDependencyInjector()->get('ride\\library\\router\\Router');
-    $url = $router->getRouteContainer()->getUrl($app['url']['script'], $id, $parameters);
+    $url = $router->getRouteContainer()->getUrl($app['url']['script'], $id, $parameters, $queryParameters, $querySeparator);
 
     if ($var == null) {
         return $url;
