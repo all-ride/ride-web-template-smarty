861a32500fc095ee9dc44102ea7251599a54e942
Kaya <kayalion@kayalion.net>
2016-10-15 01:27:05 +0200

    make sure the test function is not defined twice

--- src/ride/web/template/smarty/plugin/block.isGranted.php
+++ src/ride/web/template/smarty/plugin/block.isGranted.php
@@ -20,48 +20,50 @@ function smarty_block_isGranted($params, $content, &$smarty, &$repeat) {
     return $content;
 }
 
-function smarty_block_isGranted_test($params, &$smarty) {
-    if (!isset($params['route']) && !isset($params['url']) && !isset($params['permission'])) {
-        throw new Exception('No route, URL or permission provided');
-    }
+if (!function_exists('smarty_block_isGranted_test')) {
+    function smarty_block_isGranted_test($params, &$smarty) {
+        if (!isset($params['route']) && !isset($params['url']) && !isset($params['permission'])) {
+            throw new Exception('No route, URL or permission provided');
+        }
 
-    $strategy = 'OR';
-    if (isset($params['strategy'])) {
-        $strategy = strtoupper($params['strategy']);
-    }
+        $strategy = 'OR';
+        if (isset($params['strategy'])) {
+            $strategy = strtoupper($params['strategy']);
+        }
 
-    $app = $smarty->getTemplateVars('app');
-    if (!isset($app['system'])) {
-        trigger_error('System is not set in the app variable.');
-    }
+        $app = $smarty->getTemplateVars('app');
+        if (!isset($app['system'])) {
+            trigger_error('System is not set in the app variable.');
+        }
 
-    $securityManager = $app['system']->getDependencyInjector()->get('ride\\library\\security\\SecurityManager');
+        $securityManager = $app['system']->getDependencyInjector()->get('ride\\library\\security\\SecurityManager');
 
-    if ($strategy == 'AND') {
-        $isRouteAllowed = true;
-        $isUrlAllowed = true;
-        $isPermissionGranted = true;
-    } else {
-        $isRouteAllowed = false;
-        $isUrlAllowed = false;
-        $isPermissionGranted = false;
-    }
+        if ($strategy == 'AND') {
+            $isRouteAllowed = true;
+            $isUrlAllowed = true;
+            $isPermissionGranted = true;
+        } else {
+            $isRouteAllowed = false;
+            $isUrlAllowed = false;
+            $isPermissionGranted = false;
+        }
 
-    if (isset($params['route'])) {
-        $isRouteAllowed = $securityManager->isPathAllowed($params['route']);
-    }
-    if (isset($params['url'])) {
-        $isUrlAllowed = $securityManager->isUrlAllowed($params['url']);
-    }
-    if (isset($params['permission'])) {
-        $isPermissionGranted = $securityManager->isPermissionGranted($params['permission']);
-    }
+        if (isset($params['route'])) {
+            $isRouteAllowed = $securityManager->isPathAllowed($params['route']);
+        }
+        if (isset($params['url'])) {
+            $isUrlAllowed = $securityManager->isUrlAllowed($params['url']);
+        }
+        if (isset($params['permission'])) {
+            $isPermissionGranted = $securityManager->isPermissionGranted($params['permission']);
+        }
 
-    if ($strategy == 'AND') {
-        $isGranted = $isRouteAllowed && $isUrlAllowed && $isPermissionGranted;
-    } else {
-        $isGranted = $isRouteAllowed || $isUrlAllowed || $isPermissionGranted;
-    }
+        if ($strategy == 'AND') {
+            $isGranted = $isRouteAllowed && $isUrlAllowed && $isPermissionGranted;
+        } else {
+            $isGranted = $isRouteAllowed || $isUrlAllowed || $isPermissionGranted;
+        }
 
-    return $isGranted;
+        return $isGranted;
+    }
 }
