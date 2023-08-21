51ecb7c676b1d245404ce01d4f744a3b4f6771dc
Kristof <kristof@statik.be>
2023-08-21 14:57:00 +0200

    Added nesbot Carbon as dateformatter

--- src/ride/web/template/smarty/plugin/modifier.date_formatter.php
+++ src/ride/web/template/smarty/plugin/modifier.date_formatter.php
@@ -5,7 +5,6 @@
  * @package    Smarty
  * @subpackage PluginsModifier
  */
-
 /**
  * Smarty date_format modifier plugin
  * Type:     modifier
@@ -27,36 +26,20 @@
  * @return string |void
  * @uses   smarty_make_timestamp()
  */
-function smarty_modifier_date_formatter($string, $format = null, $default_date = '', $formatter = 'auto')
-{
+function smarty_modifier_date_formatter($string, $format = null) {
     if ($format === null) {
-        $format = Smarty::$_DATE_FORMAT;
+        $format = 'd/m/Y';
     }
-    /**
-     * require_once the {@link shared.make_timestamp.php} plugin
-     */
-    static $is_loaded = false;
-    if (!$is_loaded) {
-        if (!is_callable('smarty_make_timestamp')) {
-            include_once SMARTY_PLUGINS_DIR . 'shared.make_timestamp.php';
-        }
-        $is_loaded = true;
+    if (empty($string)) {
+        $string = time();
     }
-    if (!empty($string) && $string !== '0000-00-00' && $string !== '0000-00-00 00:00:00') {
-        $timestamp = smarty_make_timestamp($string);
-    } elseif (!empty($default_date)) {
-        $timestamp = smarty_make_timestamp($default_date);
-    } else {
-        return;
-    }
-    $datefmt = datefmt_create(
-        Locale::getDefault(),
-        \IntlDateFormatter::NONE,
-        \IntlDateFormatter::NONE,
-        date_default_timezone_get(),
-        \IntlDateFormatter::GREGORIAN,
-        $format
-    );
 
-    return datefmt_format($datefmt, $string);
+    try {
+        $dateCarbon = new \Carbon\Carbon($string);
+        return $dateCarbon->locale(Locale::getDefault())->isoFormat($format);
+
+    } catch (\Exception $e) {
+        return $string;
+    }
+
 }
